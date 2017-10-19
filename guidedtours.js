24f06a3e16113d801d024678e82345a44c84b657
Adam Bugaj <adampeter.bugaj@gmail.com>
2017-10-19 13:19:48 +0200

    Click the button to buy a tour

---
+++ guidedtours.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   $('ul').on('click', 'button', function(){
+      var message = $('<ol class="breadcrumb"><li class="breadcrumb-item active" style="color:#3CB371">Success! You have booked the offer travel!</li></ol>');
+       $(this).closest('.tour').append(message);
+       $(this).removeClass('details');
+   });
+});
